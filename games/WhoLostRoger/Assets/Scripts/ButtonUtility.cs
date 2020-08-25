e02a5be8e25e8d49294b572683773e3f526a61ee
Dawen <dawen.wang@uqconnect.edu.au>
2020-08-25 21:15:28 +1000

    Created button utilites for the game

---
+++ games/WhoLostRoger/Assets/Scripts/ButtonUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonUtility : MonoBehaviour
+{
+    public void doLoadScene(string sceneName)
+    {
+        Debug.Log(string.Format("Load '{0}' scene", sceneName));
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void doExit()
+    {
+        Debug.Log("Game quit.");
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
