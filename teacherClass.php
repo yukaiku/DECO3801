68f7ddc58e6405d8209ab4bae6e46ddb1dec77ef
yukaiku <yukai.ku@uq.net.au>
2020-08-28 14:56:32 +1000

    added teacherClass page

--- teacherClass.php
+++ teacherClass.php
@@ -7,7 +7,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Teacher Add Class</title>
+    <title>Teacher Class</title>
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -78,53 +78,8 @@
 
         <div role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
             <div class="row" id="schoolName">
-                <h1><b>School: </b> U Q HI</h1>
+                <h1><b>Class: </b> X X</h1>
             </div>
-            <form>
-                <div class="form-row row">
-                    <div class="col-lg-6">
-                        Username:
-                        <input type="text" class="text-input--underbar width-half" name="username" placeholder="Username" value="">
-                    </div>
-                    <div class="col-lg-3">
-                        Grade:
-                        <select class="text-input--underbar width-half" name="grade">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                        </select>
-                    </div>
-                    <div class="col-lg-3">
-                        Class:
-                        <select class="text-input--underbar width-half" name="class">
-                            <option>A</option>
-                            <option>B</option>
-                            <option>C</option>
-                            <option>D</option>
-                        </select>
-                    </div>
-
-
-
-                </div>
-                <div class="form-row row">
-                    <div class="col-lg-6">
-                        First Name:
-                        <input type="text" class="text-input--underbar width-half" name="firstName" placeholder="First" value="">
-                    </div>
-                    <div class="col-lg-6">
-                        Last Name:
-                        <input type="text" class="text-input--underbar width-half" name="lastName" placeholder="Last" value="" style="border-width-left: 1px">
-                    </div>
-                </div>
-                <hr>
-                <div clas="form-row" style="text-align: center">
-                    <button type="submit" class="btn btn-primary mb-2" style="text-align: center">Confirm</button>
-                </div>
-
-            </form>
-
             <div class="table-responsive">
                 <table class="table table-striped table-sm">
                     <thead>
@@ -154,6 +109,31 @@
                     </tbody>
                 </table>
             </div>
+            <hr>
+            <h2>Add new class member</h2>
+            <form>
+                <div class="form-row row">
+                    <div class="col-lg-3">
+                        Username:
+                        <input type="text" class="text-input--underbar width-half" name="username" placeholder="Username" value="">
+                    </div>
+                </div>
+                <div class="form-row row">
+                    <div class="col-lg-3">
+                        First Name:
+                        <input type="text" class="text-input--underbar width-half" name="firstName" placeholder="First" value="">
+                    </div>
+                    <div class="col-lg-3">
+                        Last Name:
+                        <input type="text" class="text-input--underbar width-half" name="lastName" placeholder="Last" value="" style="border-width-left: 1px">
+                    </div>
+                    <div class="col-lg-3">
+                        <button type="submit" class="btn btn-primary mb-2" style="text-align: center">Confirm</button>
+                    </div>
+                </div>
+                <hr>
+
+            </form>
             <div id="mainFooter" style="bottom:0; position: fixed;">
                 <a class="btn btn-primary mb-2" style="text-align: center" href="teacherMain.php">Back</a>
             </div>
