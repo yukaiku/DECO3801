0e97d6f73fe4c3cd7bf7da182c1fcc1e5c1b4f3a
Ruihan Chin <ruihan.chin@uqconnect.edu.au>
2020-08-31 11:45:25 +1000

    style sheet merge & some student side

---
+++ platform/studentMain.php
@@ -0,0 +1,101 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Teacher Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container-fluid">
+    <div class="row">
+        <?php
+        include_once("studentSideBar.php");
+        ?>
+        <div role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+            <div class="row" id="schoolName">
+                <h1><b>School: </b> U Q HI</h1>
+            </div>
+            <div class="row" id="searchbar-row">
+                <div class="col-lg-6">
+                    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
+                </div>
+                <div class="col-lg-3"></div>
+                <div class="col-lg-3">
+                    <ul class="navbar-nav px-3">
+                        <li class="nav-item text-nowrap">
+                            <a type="button" class="btn btn-primary" href="teacherAdd.php">Add Class</a>
+                        </li>
+                    </ul></div>
+            </div>
+
+
+            <h2>Section title</h2>
+            <div class="table-responsive">
+                <table class="table table-striped table-sm">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                        <th>Header</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>1,001</td>
+                        <td>Lorem</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+                    <tr>
+                        <td>1,001</td>
+                        <td>Lorem</td>
+                        <td>ipsum</td>
+                        <td>dolor</td>
+                        <td>sit</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/jquery-3.5.0.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script>
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.maxHeight){
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        });
+    }
+</script>
+
+</body>
+</html>
