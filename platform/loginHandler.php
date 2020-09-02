d8376ea42d2133c6f52db4cd40c19fe8582ec0b3
yukaiku <yukai.ku@uq.net.au>
2020-09-02 18:55:55 +1000

    updated loginhandler

--- platform/loginHandler.php
+++ platform/loginHandler.php
@@ -20,19 +20,18 @@ if (!mysqli_connect_errno()) { // connection to database is successful
 
     $result = mysqli_query ($connection, $sqlQueryStr); // execute the SQL query
     $row = mysqli_fetch_array($result);
-
+    /*====================================Student LOGIN HANDLER=============================================*/
     if($row != ""){ //has record of student
         $status = $row['status'];
         if($status != 0 ){ //user needs to verify account
             header('Location: verifyAccount.php?id='.$row['id'].'&username='.$username.'&pwd='.$password); // redirect to the verification page.
         }else{
             $sqlQueryStr =
-                "SELECT id, school, firstname, lastname, username, nickname, AES_DECRYPT(pwd,'deco2800') as password, grade, class, status " .
+                "SELECT id, school, firstname, lastname, username, nickname, AES_DECRYPT(pwd,'deco3801') as password, profileImage, grade, class, status " .
                 "FROM student A " .
                 "WHERE A.username = '{$username}' AND " .
                 "A.status = 0 AND " .
-                "A.password = AES_ENCRYPT('{$password}','deco2800') ";
-
+                "A.pwd = AES_ENCRYPT('{$password}','deco3801') ";
             $result = mysqli_query ($connection, $sqlQueryStr); // execute the SQL query
             if ($row = mysqli_fetch_array($result)) { // fetch the record
                 $_SESSION['user'] = $row; // put the record into the session
@@ -42,18 +41,19 @@ if (!mysqli_connect_errno()) { // connection to database is successful
                     setcookie("user", json_encode(""), time()-3600, "/");
                 }
                 mysqli_close($connection); // close database connection
-                header('Location: index.php?welcomeMessage=Welcome '. $row['firstName']); // redirect to the homepage.
+                header('Location: studentMain.php?welcomeMessage=Welcome '. $row['firstName']); // redirect to the homepage.
             } else {
                 mysqli_close($connection); // close database connection
-                header('Location: index.php?error=Invalid username or password'); // redirect to the login page.
+                header('Location: index.php?error=Invalid username or password for student'); // redirect to the login page.
             }
         }
+        /*====================================TEACHER LOGIN HANDLER=============================================*/
     }else{ //does not have record of student will go try teacher login
         $sqlQueryStr =
             "SELECT * " .
             "FROM teacher A " .
             "WHERE A.username = '{$username}' AND " .
-            "A.pwd = AES_ENCRYPT('{$password}','deco2800') ";
+            "A.pwd = AES_ENCRYPT('{$password}','deco3801') ";
 
         $result = mysqli_query ($connection, $sqlQueryStr); // execute the SQL query
         $row = mysqli_fetch_array($result);
@@ -64,12 +64,11 @@ if (!mysqli_connect_errno()) { // connection to database is successful
                 header('Location: verifyAccount.php?id='.$row['id'].'&username='.$username.'&pwd='.$password); // redirect to the verification page.
             }else{
                 $sqlQueryStr =
-                    "SELECT id, school, firstname, lastname, username, nickname, AES_DECRYPT(pwd,'deco2800') as password, grade, class, status " .
-                    "FROM student A " .
+                    "SELECT id, school, firstname, lastname, username, profileImage, AES_DECRYPT(pwd,'deco3801') as password, status " .
+                    "FROM teacher A " .
                     "WHERE A.username = '{$username}' AND " .
                     "A.status = 0 AND " .
-                    "A.password = AES_ENCRYPT('{$password}','deco2800') ";
-
+                    "A.pwd = AES_ENCRYPT('{$password}','deco3801') ";
                 $result = mysqli_query ($connection, $sqlQueryStr); // execute the SQL query
                 if ($row = mysqli_fetch_array($result)) { // fetch the record
                     $_SESSION['user'] = $row; // put the record into the session
@@ -82,11 +81,11 @@ if (!mysqli_connect_errno()) { // connection to database is successful
                     header('Location: teacherMain.php?welcomeMessage=Welcome '. $row['firstName']); // redirect to the homepage.
                 } else {
                     mysqli_close($connection); // close database connection
-                    header('Location: index.php?error=Invalid username or password'); // redirect to the login page.
+                    header('Location: index.php?error=Invalid username or password teacher'); // redirect to the login page.
                 }
             }
         }else{
-            header('Location: index.php?error=Invalid username or password'); // redirect to the login page.
+            header('Location: index.php?error=No record of user exists in database'); // redirect to the login page.
         }
     }
 
