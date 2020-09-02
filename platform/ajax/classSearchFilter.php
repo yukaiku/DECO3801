ba2c5c9b9072f1c0f0816f5c1d78aff910a29cd8
yukaiku <yukai.ku@uq.net.au>
2020-09-02 21:09:50 +1000

    Added class search filter

---
+++ platform/ajax/classSearchFilter.php
@@ -0,0 +1,22 @@
+<?php
+require_once "../includes/dbStudent.php";
+$searchItem = isset($_POST['search']) ? $_POST['search'] : "";
+$whereSql = "Where status = 0 ";
+if($searchItem == ""){
+    echo "";
+}
+if($searchItem != ""){
+    $whereSql .= " and (firstname LIKE '%". $searchItem . "%' OR lastname LIKE '%". $searchItem . "%' )";
+}
+
+$sql = "Select grade, class from student ". $whereSql . " group by grade, class order by grade, class desc";
+$resultSet = query($sql);
+$resultArray = array();
+while ($row = fetch_array($resultSet)) {
+    array_push($resultArray,array(
+        "grade"=>$row['grade'],
+        "class"=>$row['class']
+    ));
+}
+$resultJSON = json_encode($resultArray);
+print_r($resultJSON);
