6c81045b92b17e0e8673b5710f6b580e04118405
yukaiku <yukai.ku@uq.net.au>
2020-09-20 13:55:08 +1000

    Added comment in updateuser.php '

--- platform/ajax/updateUser.php
+++ platform/ajax/updateUser.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../includes/dbStudent.php';
 
-
+//used in teacher student to reset password
 $updateUser = setStudentAttributes($_POST);
 
 //$value = addslashes(file_get_contents($_FILES['profileImage']["tmp_name"]));
