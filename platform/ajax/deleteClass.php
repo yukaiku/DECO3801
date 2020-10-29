31fe596647652298383eb6e39b269851149a93f0
Yu Kai Ku <yukai.ku@uq.net.au>
2020-10-29 01:44:02 +0000

    Update deleteClass.php

--- platform/ajax/deleteClass.php
+++ platform/ajax/deleteClass.php
@@ -25,6 +25,10 @@ if($deleteArray != ""){
         }
         $sql = "Delete from student {$deleteSql}";
         $result = query ($sql); // execute the SQL query
+
+        //Delets all record of game progress, add one new statement per new game
+        $sql = "DELETE wlr FROM who_lost_roger wlr LEFT JOIN student s ON s.id = wlr.studentid WHERE s.id IS NULL";
+        $result = query($sql);
         print_r("Record Updated");
         close_connection();
     }
